a420d521879b74d76f3a93354c28181234391c60
MohamedRamdan1 <medo44448888@gmail.com>
2024-04-05 17:46:22 +0200

    UX improved - Enhanced browser support checking

--- app/page.tsx
+++ app/page.tsx
@@ -7,23 +7,25 @@ import BrowserNotSupported from '@/components/browserNotSupported'
 import Footer from '@/components/footer'
 import { AzkarProvider } from '@/contexts/AzkarProvider'
 import Contribute from '@/components/contribute'
+import DarkMode from '@/components/darkmode'
 
 export default function Home() {
-  const [hasBrowserSupport, setHasbrowserSupport] = useState(false)
+  const [hasBrowserSupport, setHasbrowserSupport] = useState(true)
 
   useEffect(() => {
-    if ('webkitSpeechRecognition' in window) {
-      setHasbrowserSupport(true)
+    if (!('webkitSpeechRecognition' in window)) {
+      setHasbrowserSupport(false)
     }
   }, [])
 
   return (
-    <main className="flex items-center justify-center h-screen w-screen bg-gray-200 mx-auto">
-      <div className="w-min whitespace-nowrap mb-20 flex flex-col gap-3">
+    <main className="flex items-center transition-all duration-300 justify-center h-screen w-screen bg-gray-200 dark:bg-zinc-900 dark:text-gray-200 mx-auto">
+      <div className="w-min whitespace-nowrap mb-20 flex flex-col gap-3 m-4">
         <AzkarProvider>
           <SebhaProvider>
             {hasBrowserSupport ? (
               <>
+                <DarkMode />
                 <Header />
                 <Sebha />
                 <Footer />
