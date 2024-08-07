20352af9f0ec3990d0c0b7ecddcd6519d5ba09bc
muhammadramadan <medo44448888@gmail.com>
2024-08-07 07:56:51 +0300

    chore: Update global CSS variables for colors

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
-    <div className="rounded-3xl rounded-b-xl text-2xl dark:bg-zinc-700/50 bg-white py-4 px-4 w-full text-center">
-      <h1 dir="rtl" className="text-balance text-lg">
+    <div className="w-full rounded-3xl rounded-b-xl bg-light-primary px-4 py-4 text-center text-2xl dark:bg-dark-secondary">
+      <h1 className="text-balance text-lg">
         ﴿وَالذّاكِرينَ اللهَ كَثيرًا وَالذّاكِراتِ أَعَدَّ اللهُ لَهُم مَغفِرَةً
         وَأَجرًا عَظيمًا﴾
       </h1>
     </div>
-  )
+  );
 }
