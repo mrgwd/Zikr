b6500ce282762027338e307a0b662350925850d4
muhammadramadan <medo44448888@gmail.com>
2024-08-11 18:08:39 +0300

    chore: Update footer styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,12 @@
-import { hijriDate } from '@/utils/hijriDate'
-import Statistics from './statistics'
+import { hijriDate } from "@/utils/hijriDate";
+import Statistics from "./statistics";
 
 export default function Footer() {
   return (
-    <div
-      className="rounded-3xl rounded-t-xl bg-white dark:bg-zinc-700/50 py-8 px-8 sm:px-12 text-center gap-3 sm:gap-2 flex justify-between font-medium text-lg"
-      dir="rtl"
-    >
+    <div className="relative flex justify-between gap-3 rounded-3xl rounded-t-xl bg-light-primary px-8 py-8 text-center text-lg font-medium dark:bg-dark-secondary sm:gap-2 sm:px-12">
       <p>{hijriDate}</p>
-      <span className="text-zinc-400">•</span>
+      <span className="text-light-muted dark:text-dark-muted">•</span>
       <Statistics />
     </div>
-  )
+  );
 }
