0e088a667ac009c268c82872ee253df9e1094998
muhammadramadan <medo44448888@gmail.com>
2024-08-11 18:41:33 +0300

    chore: Add step types

--- interfaces/appTypes.tsx
+++ interfaces/appTypes.tsx
@@ -1,23 +1,37 @@
 export interface SebhaContextValue {
-  isListening: boolean
-  counter: number
-  selectedZikr: string
-  transcript: string
-  setSelectedZikr: React.Dispatch<React.SetStateAction<string>>
-  handleStartListening: () => void
-  handleEndListening: () => void
+  isListening: boolean;
+  counter: number;
+  selectedZikr: string;
+  transcript: string;
+  setSelectedZikr: React.Dispatch<React.SetStateAction<string>>;
+  handleStartListening: () => void;
+  handleEndListening: () => void;
 }
 
 export interface SebhaProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export interface Zikr {
-  name: string
-  count: number
+  name: string;
+  count: number;
 }
 // type HexColor = `#${string & { length: 3 | 6 }}`
 export interface IconProps {
-  className?: string
-  color?: string
+  className?: string;
+  color?: string;
+}
+
+export interface step {
+  title: string;
+  description: string;
+  image?: string;
+}
+
+export interface StepperProps {
+  steps: step[];
+  currentStep: number;
+  version: string;
+  setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+  handleClose: () => void;
 }
