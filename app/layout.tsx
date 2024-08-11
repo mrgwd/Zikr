1eb02e194366e885dd3162f00415a94d0db8a874
muhammadramadan <medo44448888@gmail.com>
2024-08-11 18:13:16 +0300

    chore: Update metadata and add pwa setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,56 @@
-import type { Metadata } from 'next'
-import { IBM_Plex_Sans_Arabic } from 'next/font/google'
-import './globals.css'
-import { Analytics } from '@vercel/analytics/react'
+import type { Metadata, Viewport } from "next";
+import { IBM_Plex_Sans_Arabic } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const ibm_plex_sans_arabic = IBM_Plex_Sans_Arabic({
-  weight: '400',
-  subsets: ['arabic'],
-})
+  weight: "400",
+  subsets: ["arabic"],
+});
 
 export const metadata: Metadata = {
-  title: 'Zikr: A voice counter',
+  manifest: "/manifest.json",
+  title: "Zikr AI: A Smart sebha",
   description:
-    'A simple tool to help you count your zikr while working, studying, or focusing on something else.',
-  metadataBase: new URL('https://zikr-ptbddny9w-muhammadr.vercel.app'),
+    "A smart sebha that uses AI to count your zikr for you while you are working, studying, or focusing on something else. You say the zikr, and Zikr will count it for you.",
+  keywords: [
+    "zikr",
+    "zikr AI",
+    "sebha",
+    "AI sebha",
+    "counter",
+    "tasbeeh",
+    "voice zikr",
+    "count my zikr",
+    "zikr assistant",
+    "zikr counter",
+    "zikr app",
+    "zikr counter app",
+    "voice sebha",
+    "smart zikr",
+    "smart sebha",
+  ],
+  metadataBase: new URL("https://zikrvoice.vercel.app"),
   openGraph: {
-    images: '/opengraph-image.jpg',
+    images: "/opengraph-image.jpg",
   },
-}
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" dir="rtl">
       <body className={ibm_plex_sans_arabic.className}>
         {children}
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
