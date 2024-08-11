977125e1e45be966d8deb24146968bbd4fcbb11e
muhammadramadan <medo44448888@gmail.com>
2024-08-11 18:40:28 +0300

    chore: Refactor SebhaProvider component

--- contexts/SebhaProvider.tsx
+++ contexts/SebhaProvider.tsx
@@ -1,111 +1,108 @@
-'use client'
-import { createContext, useEffect, useState } from 'react'
-import { SebhaContextValue, SebhaProviderProps } from '@/interfaces/appTypes'
-import { useAzkar } from './AzkarProvider'
+"use client";
+import { createContext, useEffect, useState } from "react";
+import { SebhaContextValue, SebhaProviderProps } from "@/interfaces/appTypes";
+import { useAzkar } from "./AzkarProvider";
 
-let SpeechRecognition: any = null
-//let hasBrowserSupport: boolean
-let transcript: string
+let SpeechRecognition: /*SpeechRecognition | null*/ any = null;
+let transcript: string;
 
 const SebhaContext = createContext<SebhaContextValue>({
   counter: 0,
-  //hasBrowserSupport: !!SpeechRecognition,
-  selectedZikr: '',
+  selectedZikr: "",
   isListening: false,
-  transcript: '',
+  transcript: "",
   setSelectedZikr: () => {},
   handleStartListening: () => {},
   handleEndListening: () => {},
-})
+});
 
 const SebhaProvider = ({ children }: SebhaProviderProps) => {
-  const [azkar, setAzkar] = useAzkar()
-  const [counter, setCounter] = useState(0)
-  const [selectedZikr, setSelectedZikr] = useState('سبحان الله وبحمده')
-  //const [hasBrowserSupport, setHasBrowserSupport] = useState(false)
-  const [isListening, setIsListening] = useState(false)
-  const handleResetCounter = () => {
-    setCounter(0)
-  }
-  const handleStartListening = () => {
-    console.log('Start Listening for', selectedZikr)
-    SpeechRecognition.start()
-    setIsListening(true)
-  }
-  const handleEndListening = () => {
-    console.log('Listening has endded', selectedZikr)
-    //azkar.map((zikr) => zikr.name === selectedZikr && (zikr.count += counter))
+  const [azkar, setAzkar] = useAzkar();
+  const [counter, setCounter] = useState(0);
+  const [selectedZikr, setSelectedZikr] = useState("سبحان الله وبحمده");
+  const [isListening, setIsListening] = useState(false);
 
-    handleResetCounter()
-    SpeechRecognition.stop()
-    setIsListening(false)
+  const handleResetCounter = () => {
+    setCounter(0);
+  };
+  const handleStartListening = () => {
+    console.log("Start Listening for", selectedZikr);
+    SpeechRecognition.start();
+    setIsListening(true);
+  };
+  const handleEndListening = () => {
+    console.log("Listening has endded", selectedZikr);
+    handleResetCounter();
+    SpeechRecognition.stop();
+    setIsListening(false);
     setAzkar(
       azkar.map((zikr) =>
         zikr.name === selectedZikr
           ? { ...zikr, count: zikr.count + counter }
           : zikr,
       ),
-    )
-  }
+    );
+  };
+
   useEffect(() => {
-    if ('webkitSpeechRecognition' in window) {
-      SpeechRecognition = new webkitSpeechRecognition()
-      SpeechRecognition.lang = 'ar'
-      //setHasBrowserSupport(true)
+    if ("webkitSpeechRecognition" in window) {
+      SpeechRecognition = new webkitSpeechRecognition();
+      SpeechRecognition.lang = "ar";
     }
-  }, [])
+  }, []);
   useEffect(() => {
-    if (!SpeechRecognition) return
+    if (!SpeechRecognition) return;
     SpeechRecognition.onresult = (event: SpeechRecognitionEvent) => {
-      transcript = event.results[0][0].transcript
-      DetectZikr_Model1()
-    }
-  }, [selectedZikr])
+      transcript = event.results[0][0].transcript;
+      DetectZikr_Model1();
+    };
+  }, [selectedZikr]);
+
   const DetectZikr_Model1 = () => {
-    let newCounter: number = 0
+    let newCounter: number = 0;
     switch (selectedZikr) {
-      case 'سبحان الله وبحمده':
-        newCounter = transcript.split('سبحان').length - 1
-        break
+      case "سبحان الله وبحمده":
+        newCounter = transcript.split("سبحان").length - 1;
+        break;
 
-      case 'استغفر الله':
-        newCounter = transcript.split('ستغفر').length - 1
-        break
+      case "استغفر الله":
+        newCounter = transcript.split("ستغفر").length - 1;
+        break;
 
-      case 'لا إله إلا الله':
-        newCounter = transcript.split('الله').length - 1
-        break
+      case "لا إله إلا الله":
+        newCounter = transcript.split("الله").length - 1;
+        break;
 
-      case 'لا حول ولا قوة إلا بالله':
-      case 'لا حول ولا قوه الا بالله':
-        newCounter = transcript.split('ولا قو').length - 1
-        break
+      case "لا حول ولا قوة إلا بالله":
+      case "لا حول ولا قوه الا بالله":
+        newCounter = transcript.split("ولا قو").length - 1;
+        break;
 
-      case 'اللهم صلِّ وسلم على محمد':
-        newCounter = transcript.split('وسلم ع').length - 1
-        break
+      case "اللهم صلِّ وسلم على محمد":
+        newCounter = transcript.split("وسلم ع").length - 1;
+        break;
 
-      case 'الله أكبر':
-      case 'الله اكبر':
-        newCounter = transcript.split('كبر').length - 1
-        break
+      case "الله أكبر":
+      case "الله اكبر":
+        newCounter = transcript.split("كبر").length - 1;
+        break;
 
       default:
-        newCounter = transcript.split(selectedZikr).length - 1
+        newCounter = transcript.split(selectedZikr).length - 1;
     }
 
-    setCounter((counter) => counter + newCounter)
-    console.log(newCounter, selectedZikr, transcript, azkar)
-  }
+    setCounter((counter) => counter + newCounter);
+    console.log(newCounter, selectedZikr, transcript, azkar);
+  };
+
   if (SpeechRecognition)
     SpeechRecognition.onend = () => {
-      if (isListening) SpeechRecognition.start()
-    }
+      if (isListening) SpeechRecognition.start();
+    };
   return (
     <SebhaContext.Provider
       value={{
         counter,
-        //hasBrowserSupport,
         selectedZikr,
         isListening,
         transcript,
@@ -116,6 +113,6 @@ const SebhaProvider = ({ children }: SebhaProviderProps) => {
     >
       {children}
     </SebhaContext.Provider>
-  )
-}
-export { SebhaContext, SebhaProvider }
+  );
+};
+export { SebhaContext, SebhaProvider };
