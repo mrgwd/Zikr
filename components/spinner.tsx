91a063e3d798bc79b5eafd189364077a38eec26c
muhammadramadan <medo44448888@gmail.com>
2024-08-11 18:30:00 +0300

    chore: Add Spinner component

---
+++ components/spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="animate-spin rounded-full border-4 border-light-primary border-t-primary p-2"></div>
+    </div>
+  );
+}
