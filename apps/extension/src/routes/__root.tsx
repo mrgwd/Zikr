be84fde69a4186f21860bc7af18eff476a441eec
mohamedramadan <mogdwd@gmail.com>
2026-03-22 23:01:06 +0200

    feat: integrate NextThemesProvider for theme management in root route

--- apps/extension/src/routes/__root.tsx
+++ apps/extension/src/routes/__root.tsx
@@ -1,9 +1,16 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { ThemeProvider as NextThemesProvider } from "@workspace/ui/components/theme-provider";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      enableColorScheme
+    >
       <Outlet />
-    </>
+    </NextThemesProvider>
   ),
 });
