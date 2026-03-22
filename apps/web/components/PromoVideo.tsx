a94faaec49106caf761f1c73ea83bf580883461f
mohamedramadan <mogdwd@gmail.com>
2026-03-22 23:05:45 +0200

    fix: update PromoVideo component to display new demo video and improve animation effects

--- apps/web/components/PromoVideo.tsx
+++ apps/web/components/PromoVideo.tsx
@@ -1,19 +1,17 @@
 export default function PromoVideo() {
-  // will render iframe from youtube
   return (
-    <div className="">
-      <div className="bg-muted border-border aspect-video overflow-hidden rounded-xl border shadow-lg md:rounded-3xl md:shadow-2xl">
-        <p className="pt-12 text-center"> Demo is coming soon</p>
-        {/* <iframe
+    <div className="animate-fade opacity-0" style={{ animationDelay: "300ms" }}>
+      <div className="border-border aspect-video overflow-hidden rounded-xl border shadow-lg md:rounded-3xl md:shadow-2xl">
+        <iframe
           width="100%"
           height="100%"
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          src="https://www.youtube.com/embed/xTEgljBI_aE"
           title="Katheera Demo Video"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           className="h-full w-full"
-        /> */}
+        />
       </div>
     </div>
   );
