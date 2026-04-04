0f331567263a4847a32d726da26322fa8159ec1a
mohamedramadan <mogdwd@gmail.com>
2026-04-05 01:31:55 +0200

    refactor: replace ZikrInfoCard with ZikrInfoList for improved data presentation; add generateStaticParams function for dynamic routing

--- apps/web/app/app/zikr/[slug]/page.tsx
+++ apps/web/app/app/zikr/[slug]/page.tsx
@@ -1,11 +1,19 @@
 import { JSX } from "react";
 import { getZikrData } from "@workspace/azkar/helpers";
 import { ZikrInfo } from "@workspace/azkar/types";
-import { ArrowRight, Copy } from "@workspace/ui/index";
+import { ArrowRight } from "@workspace/ui/index";
 import { Button } from "@workspace/ui/components/button";
-import ZikrInfoCard from "@workspace/ui/layout/ZikrInfoCard";
-import CopyButton from "@workspace/ui/layout/CopyButton";
+import ZikrInfoList from "@workspace/ui/layout/ZikrInfoList";
 import Link from "next/link";
+import { SupportedAzkar } from "@workspace/azkar/constants";
+
+export async function generateStaticParams() {
+  const slugs = SupportedAzkar.map((z) => z.id);
+  return slugs.map((slug) => ({
+    slug,
+  }));
+}
+
 export default async function HadithPage({
   params,
 }: {
@@ -15,34 +23,13 @@ export default async function HadithPage({
   const data: ZikrInfo[] = await getZikrData(slug);
   return (
     <div className="space-y-2">
-      <Button
-        variant="ghost"
-        className="my-4"
-        render={<Link href="/app"></Link>}
-      >
-        <ArrowRight />
-        عودة
-      </Button>
-      {data.map((item, index: number) => (
-        <ZikrInfoCard
-          key={item.id}
-          className="animate-fade-down opacity-0"
-          style={{
-            animationDelay: `${(index + 1) * 50}ms`,
-          }}
-        >
-          <ZikrInfoCard.Text>{item.text}</ZikrInfoCard.Text>
-          <ZikrInfoCard.Footer>
-            {item.source}
-            <CopyButton
-              variant="ghost"
-              toBeCopied={`${item.text} \n- ${item.source}`}
-            >
-              <Copy />
-            </CopyButton>
-          </ZikrInfoCard.Footer>
-        </ZikrInfoCard>
-      ))}
+      <Link href="/app">
+        <Button variant="ghost" className="my-4">
+          <ArrowRight />
+          عودة
+        </Button>
+      </Link>
+      <ZikrInfoList zikrInfoList={data} LinkComponent={Link} />
     </div>
   );
 }
