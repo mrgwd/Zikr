1fca1d51b3c1099922102a626958b13cf368204e
mohamedramadan <mogdwd@gmail.com>
2026-05-02 20:48:41 +0300

    refactor: replace ModeToggle with SettingsPanel

--- packages/ui/src/layout/TopBar.tsx
+++ packages/ui/src/layout/TopBar.tsx
@@ -1,11 +1,9 @@
-import { ModeToggle } from "./ModeToggle";
+import SettingsPanel from "./SettingsPanel";
 
 export function TopBar() {
   return (
-    <div className="relative">
-      <div className="absolute top-0 z-50 flex w-full -translate-y-2 justify-center rounded-full pt-2 opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
-        <ModeToggle />
-      </div>
+    <div>
+      <SettingsPanel />
     </div>
   );
 }
